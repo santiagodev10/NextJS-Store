321dbb0e94ba0ae608a03f3fc01e7fb742863c8a
santiagodev <sanxp18@gmail.com>
2026-03-02 16:19:39 -0400

    install sass dependencies and sass options configurations to have global styles and scss variables to use for

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  reactCompiler: true,
+  // reactCompiler: true,
+  sassOptions: {
+    includePaths: [path.join(process.cwd(), 'src', 'scss')],
+    // Se usa ruta absoluta para evitar errores de ruteo con Turbopack
+    prependData: `@import "${path.join(process.cwd(), 'src', 'scss', 'main.scss')}";`,
+  }
 };
 
 export default nextConfig;
