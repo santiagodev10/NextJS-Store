986d024752979d0f4992086f3287cd101f4174c9
santiagodev <sanxp18@gmail.com>
2026-03-02 16:57:53 -0400

    add global.scss styles and import it from the root layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
 import { Header } from "@/components/shared/Header";
 import { Footer } from "@/components/shared/Footer";
+//No hace falta importar los estilos globales como en css modules, ya que Next JS los aplica automáticamente a toda la aplicación
+import "@/scss/global.scss";
 
 export const metadata = {
   title: "Next JS store",
