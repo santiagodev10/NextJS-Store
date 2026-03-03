8da66d2c766f4a744d7870413b16ac028d040b10
santiagodev <sanxp18@gmail.com>
2026-03-03 19:25:23 -0400

    add hero image from public folder

--- src/components/home/Description/Description.jsx
+++ src/components/home/Description/Description.jsx
@@ -1,7 +1,11 @@
+import styles from "./Description.module.scss"
+
 export const Description = () => {
     return (
         <section>
+            <img className={styles["hero-image"]} src="/images/description.jpeg" alt="product description" />
             <h2>Description</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates eum tempore, inventore id totam ullam. Officiis dolores, veniam incidunt labore eum est inventore quis adipisci, corporis doloremque voluptas perspiciatis blanditiis.</p>
         </section>
     );
 }
--- src/components/home/Hero/Hero.jsx
+++ src/components/home/Hero/Hero.jsx
@@ -3,7 +3,8 @@ import styles from './Hero.module.scss';
 export const Hero = () => {
     return (
         <section>
-            <h1 className={styles.hero}>Hero</h1>
+            <h1 className={styles.hero}>NextJS Store</h1>
+            <h2 className={styles.slogan}>Empowering Your Tomorrow, Today!</h2>
         </section>
     );
 }
