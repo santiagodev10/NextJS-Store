2c7f6711f2e454566e072dc94bbeff80cf2be547
santiagodev <sanxp18@gmail.com>
2026-03-05 17:44:55 -0400

    add roboto font from google fonts using next/font/google

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Header } from "@/components/shared/Header";
 import { Footer } from "@/components/shared/Footer";
+import { Roboto } from "next/font/google";
 //No hace falta importar los estilos globales como en css modules, ya que Next JS los aplica automáticamente a toda la aplicación
 import "@/scss/global.scss";
 
@@ -8,11 +9,17 @@ export const metadata = {
   description: "My first Next JS app",
 };
 
+const roboto = Roboto({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap"
+})
+
 //Todo lo que agreguemos en el root layout va a aparecer en TODAS las páginas de la aplicación
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <Header />
         {children}
         <Footer />
