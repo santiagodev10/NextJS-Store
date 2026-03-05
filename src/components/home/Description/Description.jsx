90a86e0306de2e7c76620cd6f31618fce4fe2e0f
santiagodev <sanxp18@gmail.com>
2026-03-05 12:16:05 -0400

    add Next Image component, and styles for the hero container section

--- src/components/home/Description/Description.jsx
+++ src/components/home/Description/Description.jsx
@@ -1,11 +1,15 @@
 import styles from "./Description.module.scss"
+import Image from "next/image"
 
 export const Description = () => {
     return (
-        <section>
-            <img className={styles["hero-image"]} src="/images/description.jpeg" alt="product description" />
-            <h2>Description</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates eum tempore, inventore id totam ullam. Officiis dolores, veniam incidunt labore eum est inventore quis adipisci, corporis doloremque voluptas perspiciatis blanditiis.</p>
+        <section className={styles["hero-container"]}>
+            {/* <img className={styles["hero-image"]} src="/images/description.jpeg" alt="product description" /> */}
+            <Image className={styles["hero-image"]} src="/images/description.jpeg" alt="product description" width={500} height={300} priority={true} quality={75} />
+            <div className={styles["description-container"]}>
+                <h2 className={styles["description-title"]}>Bring the future today!</h2>
+                <p className={styles["description-content"]}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates eum tempore, inventore id totam ullam. Officiis dolores, veniam incidunt labore eum est inventore quis adipisci, corporis doloremque voluptas perspiciatis blanditiis.</p>
+            </div>
         </section>
     );
 }
