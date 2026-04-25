235be2273d003a60f1aee71de15460174219e461
santiagodev <sanxp18@gmail.com>
2026-04-25 19:54:45 -0400

    add .env file to store shopify's enviroment variables and print it in MainProducts.jsx to see how it works

--- src/components/home/MainProducts/MainProducts.jsx
+++ src/components/home/MainProducts/MainProducts.jsx
@@ -1,4 +1,8 @@
+"use cliente";
+
 export const MainProducts = () => {
+    console.log("Variable de entorno: " + process.env.SHOPIFY_HOST_NAME);
+
     return (
         <section>
             <h2>Main Products</h2>
